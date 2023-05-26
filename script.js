3c0ae174c6d074caa88bab56a4efaa33dc00a1d8
Blank Chilanhouth <78426919+Blank-develop@users.noreply.github.com>
2023-05-26 10:53:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       // document.querySelector('.message').textContent = '💥 You lost the game!';
       displayMessage('💥 You lost the game!');
+      alert(guess);
       document.querySelector('.score').textContent = 0;
     }
   }
